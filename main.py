3404e07e0c39ea29fd6e92476ddc34c20814a26f
Bekir Erdener <b.erdener41@gmail.com>
2025-07-14 18:49:25 +0300

    İlk yükleme

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# FastAPI uygulama giriş noktası
+from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
+from app.routes import router
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+app.include_router(router)
